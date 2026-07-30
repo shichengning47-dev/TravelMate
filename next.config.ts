0130b797a8c1b655d9eb82be8e256988f440a23d
shichengning47-dev <shichengning47@gmail.com>
2026-07-30 16:32:15 +0800

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: process.env.GITHUB_ACTIONS ? "/TravelMate" : "",
+  assetPrefix: process.env.GITHUB_ACTIONS ? "/TravelMate/" : "",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
+
